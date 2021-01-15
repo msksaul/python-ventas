f4a957501f96b77a3566e4ebad5ec399fae34dd2
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-15 14:58:05 -0600

    Client Manipulation Functions

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 clients = 'pablo,ricardo,'
 
-if __name__ == '__main__':
-  clients += 'david'
+def create_client(client_name):
+  global clients
+  clients += client_name
+  _add_comma()
+
+
+def list_clients():
+  global clients
   print(clients)
+
+
+def _add_comma():
+  global clients
+  clients += ','
+
+
+if __name__ == '__main__':
+  list_clients()
+  create_client('david')
+  list_clients()
