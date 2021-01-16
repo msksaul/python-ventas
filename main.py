300d5c088b172b599c3f79939afe4d909a6a5609
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-15 18:33:33 -0600

    Update Clients

--- main.py
+++ main.py
@@ -2,21 +2,57 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
   global clients
-  clients += client_name
-  _add_comma()
 
+  if client_name not in clients:
+    clients += client_name
+    _add_comma()
+  else:
+    print('Client already in client\'s list')
+
+def update_client(client_name, update_client):
+  global clients
+
+  if client_name in clients:
+    clients = clients.replace(client_name + ',', updated_client_name + ',')
+  else:
+    print('Client is not in clients list')
 
 def list_clients():
   global clients
   print(clients)
 
-
 def _add_comma():
   global clients
   clients += ','
 
+def _print_welcome():
+  print('WELCOME TO PYTHON VENTAS')
+  print('*' * 50)
+  print('What would you want to do today? ')
+  print('[C]reate client')
+  print('[U]pdate client')
+  print('[D]elete client')
+
+def _get_client_name():
+  return input('What is the client name? ')
 
 if __name__ == '__main__':
-  list_clients()
-  create_client('david')
-  list_clients()
+  _print_welcome()
+
+  command = input()
+  command = command.upper()
+
+  if command == 'C':
+    client_name = _get_client_name()
+    create_client(client_name)
+    list_clients()
+  elif command == 'D':
+    pass
+  elif command == 'U':
+    client_name = _get_client_name()
+    updated_client_name = input('What is the updated client name? ')
+    update_client(client_name, updated_client_name)
+    list_clients()
+  else:
+    print('Invalid command')
+
