51e26b50c0a0dc11b05565e698dbe50c3c2960ba
msksaul <64514098+msksaul@users.noreply.github.com>
2021-01-15 20:01:25 -0600

    Delete Clients

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ def update_client(client_name, update_client):
   else:
     print('Client is not in clients list')
 
+def delete_client(client_name):
+  global clients
+
+  if client_name in clients:
+    clients = clients.replace(client_name + ',', '')
+  else:
+    print('Client is not in clients list')
+
 def list_clients():
   global clients
   print(clients)
@@ -47,7 +55,9 @@ if __name__ == '__main__':
     create_client(client_name)
     list_clients()
   elif command == 'D':
-    pass
+    client_name = _get_client_name()
+    delete_client(client_name)
+    list_clients()
   elif command == 'U':
     client_name = _get_client_name()
     updated_client_name = input('What is the updated client name? ')
